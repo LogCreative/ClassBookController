62405b9fece2df7ba8318c6016dece6547bd76f5
LogCreative <longlaoda123456@qq.com>
2021-01-12 22:46:05 +0800

    minor setting to settings

--- 班级图书管理器/My Project/Settings.Designer.vb
+++ 班级图书管理器/My Project/Settings.Designer.vb
@@ -57,7 +57,7 @@ Namespace My
         <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
          Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|\DatabaseCBC.mdb")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\DatabaseCBC.mdb")>  _
         Public ReadOnly Property DatabaseCBCConnectionChinese() As String
             Get
                 Return CType(Me("DatabaseCBCConnectionChinese"),String)
@@ -91,7 +91,7 @@ Namespace My
         <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
          Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|\DatabaseCBC.mdb")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\DatabaseCBC.mdb")>  _
         Public ReadOnly Property DatabaseCBCConnectionUH() As String
             Get
                 Return CType(Me("DatabaseCBCConnectionUH"),String)
